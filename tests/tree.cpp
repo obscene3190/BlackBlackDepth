5529164f2d1d973a20cee040635d2c767321168d
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-03-28 22:13:01 +0300

    Update tree.cpp

--- tests/tree.cpp
+++ tests/tree.cpp
@@ -6,24 +6,24 @@
 TEST_CASE("creating tree")
 {
 	tree_t tree;
-	REQUIRE( tree.root_ == nullptr );
+	REQUIRE( tree.root() == nullptr );
 }
 
-TEST_CASE("adding tree")
+/*TEST_CASE("adding tree")
 {
 	tree_t tree;
   
-	REQUIRE( tree.root_ == nullptr );
+	REQUIRE();
 }
 
 TEST_CASE("printing tree")
 {
 	tree_t tree;
-	REQUIRE( tree.root_ == nullptr );
+	REQUIRE(  );
 }
 
 TEST_CASE("finding tree")
 {
 	tree_t tree;
-	REQUIRE( tree.root_ == nullptr );
-}
+	REQUIRE(  );
+}*/
