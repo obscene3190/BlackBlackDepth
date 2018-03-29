60a819befd62e9cd718496d373967d65b5dbac24
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-03-29 19:12:22 +0300

    Update tree.cpp

--- tests/tree.cpp
+++ tests/tree.cpp
@@ -15,9 +15,11 @@ TEST_CASE("adding tree")
 	std::string input1{"+2/n"};
 	std::string input2{"+1/n"};
 	std::string input3{"+3/n"};
-	std::string result{"----3/n"
-			   "--2/n"
-			   "----1/n"};
+	std::string result{"----3
+	--2
+	----1
+	"};
+	
 	std::istringstream istream1{ input1 };
 	std::istringstream istream2{ input2 };
 	std::istringstream istream3{ input3 };
@@ -25,7 +27,7 @@ TEST_CASE("adding tree")
 	REQUIRE(read(tree, istream2));
 	REQUIRE(read(tree, istream3));
 	std::ostringstream ostream;
-	tree.print(ostream, tree.root(), 1);
+	tree.print(ostream, tree.root());
 	REQUIRE( result == ostream.str() );
 } 
 /*
