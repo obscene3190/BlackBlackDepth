3436df86520e8fd5fd2dce3a4a3e59034ec253a7
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-05-07 00:26:39 +0300

    Update RBT.cpp

--- tests/RBT.cpp
+++ tests/RBT.cpp
@@ -38,110 +38,110 @@ TEST_CASE( "elements can be inserted in rb tree", "[insert]" ) {
 						 "--b15\n"
 						 "----b10\n" );
     tree.insert( 20 );
-    REQUIRE( tree.print(ostream, tree.root()) == "------r85\n"
-"----b70\n"
-"------r20\n"
-"--b15\n"
-"----b10\n" );
+    REQUIRE( tree.print(ostream, tree.root()) == 	"------r85\n"
+							"----b70\n"
+							"------r20\n"
+							"--b15\n"
+							"----b10\n" );
     tree.insert( 60 );
-    REQUIRE( tree.print(ostream, tree.root()) == "------b85\n"
-"----r70\n"
-"--------r60\n"
-"------b20\n"
-"--b15\n"
-"----b10\n" );
+    REQUIRE( tree.print(ostream, tree.root()) == 	"------b85\n"
+							"----r70\n"
+							"--------r60\n"
+							"------b20\n"
+							"--b15\n"
+							"----b10\n" );
     tree.insert( 30 );
-    REQUIRE( tree.print(ostream, tree.root()) == "------b85\n"
-"----r70\n"
-"--------r60\n"
-"------b30\n"
-"--------r20\n"
-"--b15\n"
-"----b10\n" );
+    REQUIRE( tree.print(ostream, tree.root()) == 	"------b85\n"
+							"----r70\n"
+							"--------r60\n"
+							"------b30\n"
+							"--------r20\n"
+							"--b15\n"
+							"----b10\n" );
     tree.insert( 50 );
-    REQUIRE( tree.print(ostream, tree.root()) == "------b85\n"
-"----r70\n"
-"------b60\n"
-"--------r50\n"
-"--b30\n"
-"------b20\n"
-"----r15\n"
-"------b10\n" );
+    REQUIRE( tree.print(ostream, tree.root()) == 	"------b85\n"
+							"----r70\n"
+							"------b60\n"
+							"--------r50\n"
+							"--b30\n"
+							"------b20\n"
+							"----r15\n"
+							"------b10\n" );
     tree.insert( 65 );
-    REQUIRE( tree.print(ostream, tree.root()) == "------b85\n"
-"----r70\n"
-"--------r65\n"
-"------b60\n"
-"--------r50\n"
-"--b30\n"
-"------b20\n"
-"----r15\n"
-"------b10\n" );
+    REQUIRE( tree.print(ostream, tree.root()) == 	"------b85\n"
+							"----r70\n"
+							"--------r65\n"
+							"------b60\n"
+							"--------r50\n"
+							"--b30\n"
+							"------b20\n"
+							"----r15\n"
+							"------b10\n" );
     tree.insert( 80 );
-    REQUIRE( tree.print(ostream, tree.root()) == "------b85\n"
-"--------r80\n"
-"----r70\n"
-"--------r65\n"
-"------b60\n"
-"--------r50\n"
-"--b30\n"
-"------b20\n"
-"----r15\n"
-"------b10\n" );
+    REQUIRE( tree.print(ostream, tree.root()) == 	"------b85\n"
+							"--------r80\n"
+							"----r70\n"
+							"--------r65\n"
+							"------b60\n"
+							"--------r50\n"
+							"--b30\n"
+							"------b20\n"
+							"----r15\n"
+							"------b10\n" );
     tree.insert( 90 );
-    REQUIRE( tree.print(ostream, tree.root()) == "--------r90\n"
-"------b85\n"
-"--------r80\n"
-"----r70\n"
-"--------r65\n"
-"------b60\n"
-"--------r50\n"
-"--b30\n"
-"------b20\n"
-"----r15\n"
-"------b10\n" );
+    REQUIRE( tree.print(ostream, tree.root()) == 	"--------r90\n"
+							"------b85\n"
+							"--------r80\n"
+							"----r70\n"
+							"--------r65\n"
+							"------b60\n"
+							"--------r50\n"
+							"--b30\n"
+							"------b20\n"
+							"----r15\n"
+							"------b10\n" );
     tree.insert( 40 );
-    REQUIRE( tree.print(ostream, tree.root()) == "--------r90\n"
-"------b85\n"
-"--------r80\n"
-"----b70\n"
-"--------b65\n"
-"------r60\n"
-"--------b50\n"
-"----------r40\n"
-"--b30\n"
-"------b20\n"
-"----b15\n"
-"------b10\n" );
+    REQUIRE( tree.print(ostream, tree.root()) == 	"--------r90\n"
+							"------b85\n"
+							"--------r80\n"
+							"----b70\n"
+							"--------b65\n"
+							"------r60\n"
+							"--------b50\n"
+							"----------r40\n"
+							"--b30\n"
+							"------b20\n"
+							"----b15\n"
+							"------b10\n" );
     tree.insert( 5 );
-    REQUIRE( tree.print(ostream, tree.root()) == "--------r90\n"
-"------b85\n"
-"--------r80\n"
-"----b70\n"
-"--------b65\n"
-"------r60\n"
-"--------b50\n"
-"----------r40\n"
-"--b30\n"
-"------b20\n"
-"----b15\n"
-"------b10\n"
-"--------r5\n" );
+    REQUIRE( tree.print(ostream, tree.root()) == 	"--------r90\n"
+							"------b85\n"
+							"--------r80\n"
+							"----b70\n"
+							"--------b65\n"
+							"------r60\n"
+							"--------b50\n"
+							"----------r40\n"
+							"--b30\n"
+							"------b20\n"
+							"----b15\n"
+							"------b10\n"
+							"--------r5\n" );
     tree.insert( 55 );
-    REQUIRE( tree.print(ostream, tree.root()) == "--------r90\n"
-"------b85\n"
-"--------r80\n"
-"----b70\n"
-"--------b65\n"
-"------r60\n"
-"----------r55\n"
-"--------b50\n"
-"----------r40\n"
-"--b30\n"
-"------b20\n"
-"----b15\n"
-"------b10\n"
-"--------r5\n" );
+    REQUIRE( tree.print(ostream, tree.root()) == 	"--------r90\n"
+							"------b85\n"
+							"--------r80\n"
+							"----b70\n"
+							"--------b65\n"
+							"------r60\n"
+							"----------r55\n"
+							"--------b50\n"
+							"----------r40\n"
+							"--b30\n"
+							"------b20\n"
+							"----b15\n"
+							"------b10\n"
+							"--------r5\n" );
 }
 TEST_CASE( "elements can be removde from rb tree", "[remove]" ) {
     RBT<int> tree;
@@ -169,14 +169,14 @@ TEST_CASE( "elements can be removde from rb tree", "[remove]" ) {
         //            /  \
         //           /    \
         //         r3      r5
-        REQUIRE( tree.print(ostream, tree.root()) == "------r5\n"
-"----b4\n"
-"------r3\n"
-"--b2\n"
-"----b1\n" );
+        REQUIRE( tree.print(ostream, tree.root()) == 	"------r5\n"
+							"----b4\n"
+							"------r3\n"
+							"--b2\n"
+							"----b1\n" );
         //std::cout << tree << std::endl;
         tree.remove( 2 );
-        std::ofstream("tmp") << tree << std::endl;
+        //std::ofstream("tmp") << tree << std::endl;
     }
 }
 TEST_CASE( "elements can be removed from rb tree ", "[remove, bug]" ) {
@@ -184,25 +184,34 @@ TEST_CASE( "elements can be removed from rb tree ", "[remove, bug]" ) {
         RBT<int> tree;
         SECTION( "when brother is black and his sons" ) {
         RBT<int> tree;
-	tree.insert(1);
-	tree.insert(2);
-	tree.insert(6);
-	tree.insert(8);
-	tree.insert(9);
-	tree.insert(7);
-	tree.insert(4);
-	tree.remove(4);
-	tree.remove(8);
-        REQUIRE( tree.print(ostream, tree.root()) == "------b9\n"
-"----r7\n"
-"-----b6\n"
-"--b2\n"
-"----b1\n" );
+	tree.insert( 1 );
+            tree.insert( 2 );
+            tree.insert( 6 );
+            tree.insert( 8 );
+            tree.insert( 9 );
+            tree.insert( 7 );
+            tree.insert( 4 );
+            tree.insert( 4 );
+            tree.insert( 2 );
+            tree.insert( 4 );
+            tree.insert( 4 );
+            tree.remove( 4 );
+            tree.remove( 4 );
+            tree.remove( 4 );
+            tree.remove( 4 );
+            tree.remove( 8 );
+        REQUIRE( tree.print(ostream, tree.root()) == 	"----b9\n"
+							"------r7\n"
+							"--b6\n"
+							"------b2\n"
+							"----r2\n"
+							"------b1\n" );
         tree.remove( 9 );
-        REQUIRE( tree.print(ostream, tree.root()) == "----b7\n"
-"------r6\n"
-"--b2\n"
-"----b1\n" );
+        REQUIRE( tree.print(ostream, tree.root()) == 	"----b7\n"
+							"--b6\n"
+							"------b2\n"
+							"----r2\n"
+							"------b1\n" );
         }
         SECTION( "when brother is black and his right son is red" ) {
             tree.insert( 1 );
@@ -218,28 +227,28 @@ TEST_CASE( "elements can be removed from rb tree ", "[remove, bug]" ) {
             tree.insert( 11 );
             tree.insert( 0 );
             tree.remove( 3 );
-            REQUIRE( tree.print(ostream, tree.root()) == "----------r11\n"
-"--------b10\n"
-"----------r9\n"
-"------r8\n"
-"--------b7\n"
-"----b6\n"
-"------b5\n"
-"--b4\n"
-"------b2\n"
-"----b1\n"
-"------b0\n" );
+            REQUIRE( tree.print(ostream, tree.root()) =="----------r11\n"
+							"--------b10\n"
+							"----------r9\n"
+							"------r8\n"
+							"--------b7\n"
+							"----b6\n"
+							"------b5\n"
+							"--b4\n"
+							"------b2\n"
+							"----b1\n"
+							"------b0\n" );
             tree.remove( 2 );
-            REQUIRE( tree.print(ostream, tree.root()) == "--------r11\n"
-"------b10\n"
-"--------r9\n"
-"----b8\n"
-"------b7\n"
-"--b6\n"
-"------b5\n"
-"----b4\n"
-"------b1\n"
-"--------r0\n" );
+            REQUIRE( tree.print(ostream, tree.root()) =="--------r11\n"
+							"------b10\n"
+							"--------r9\n"
+							"----b8\n"
+							"------b7\n"
+							"--b6\n"
+							"------b5\n"
+							"----b4\n"
+							"------b1\n"
+							"--------r0\n" );
         }
         SECTION( "when brother is red" ) {
             tree.insert( 1 );
@@ -260,10 +269,35 @@ TEST_CASE( "elements can be removed from rb tree ", "[remove, bug]" ) {
             tree.insert( 5 );
             tree.insert( 5 );
             tree.insert( 5 );
-            REQUIRE( tree.print(ostream, tree.root()) == "r0b1b2b4b5b5b5r5b5r5r6b7b8r9b10r11" );
+            REQUIRE( tree.print(ostream, tree.root())== "------b8\n"
+							"--------b7\n"
+							"----r6\n"
+							"------------r5\n"
+							"----------b5\n"
+							"--------r5\n"
+							"----------b5\n"
+							"------b5\n"
+							"--------b5\n"
+							"--b4\n"
+							"------b2\n"
+							"----b1\n"
+							"------b0\n" );
             tree.remove( 2 );
-            std::cout << tree << std::endl;
-            REQUIRE( tree.print(ostream, tree.root()) == "r0b1b4b5r5b5b5b5r5b6b7b8r9b10r11" );
+            REQUIRE( tree.print(ostream, tree.root()) == "--------r11\n"
+							"------b10\n"
+							"--------r9\n"
+							"----b8\n"
+							"------b7\n"
+							"--b6\n"
+							"----------r5\n"
+							"--------b5\n"
+							"------b5\n"
+							"--------b5\n"
+							"----r5\n"
+							"--------b5\n"
+							"------b4\n"
+							"--------b1\n"
+							"----------r0\n" );
         }
     }
 }
